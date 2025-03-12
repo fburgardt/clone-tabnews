0df1967976e49be013ef4ecbd800022c89c2f8f5
Flavio Burgardt <f.burgardt@gmail.com>
2025-03-11 21:17:50 -0300

    exemplo de testes unitários

--- tests/caculadora.test.js
+++ tests/caculadora.test.js
@@ -1,3 +1,6 @@
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
+const calculadora = require("../models/calculadora.js");
+
+const result = calculadora.somar(2, 2);
+test("somar 2 + 2 deveria retornar 4", () => {
+  expect(result).toBe(4);
 });
