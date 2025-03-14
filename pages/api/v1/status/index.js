36a1fb9e4ed0313c5e1d8692cbc581fee445a94f
Flavio Burgardt <f.burgardt@gmail.com>
2025-03-13 21:54:34 -0300

    apis e ajustando testes

---
+++ pages/api/v1/status/index.js
@@ -0,0 +1,5 @@
+function xisto(req, res) {
+    res.status(200).json({chave: "oi cabeção"})
+}
+
+export default xisto;
--- tests/unit/caculadora.test.js
+++ tests/unit/caculadora.test.js
@@ -1,4 +1,4 @@
-const calculadora = require("../models/calculadora.js");
+const calculadora = require("../../models/calculadora.js");
 
 const result = calculadora.somar(2, 2);
 test("somar 2 + 2 deveria retornar 4", () => {
